b56c40971ab26b7a50ce3567f09b287f6becad62
Ivylucyawuor <awuorivy09>
2024-08-30 16:44:34 +0300

    Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    for i in range(2, length):
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+    print(fibonacci_sequence[:length])
+    
+print_fibonacci(9)
